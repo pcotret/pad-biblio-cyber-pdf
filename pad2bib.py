95f731c0c29fdc08804c3cbfd9523b4dd79b26ee
Pascal Cotret <pascal.cotret@gmail.com>
2022-11-06 14:58:21 +0100

    function for clean biblio

--- pad2bib.py
+++ pad2bib.py
@@ -1,3 +1,25 @@
+# Function to remove first lines. Taken from https://stackoverflow.com/questions/61681088/how-to-delete-lines-from-a-file-until-a-specific-character-python
+def erase(file_name: str, start_key: str, stop_key: str):
+    """
+    This function will delete all line from the givin start_key
+    until the stop_key. (include: start_key) (exclude: stop_key)
+    """
+    try: 
+        with open(file_name, 'r+') as fr: 
+            lines = fr.readlines()
+        with open(file_name, 'w+') as fw:
+            delete = False
+            for line in lines:
+                if line.strip('\n') == start_key:
+                     delete = True
+                elif line.strip('\n') == stop_key:
+                     delete = False
+                if not delete: 
+                    fw.write(line)
+    except RuntimeError as ex: 
+        print(f"erase error:\n\t{ex}")
+
+# Main program
 file = open("biblio.bib", "rt")
 replacement = ""
 number=-1
@@ -11,3 +33,4 @@ file.close()
 fout = open("biblio.bib", "wt")
 fout.write(replacement)
 fout.close()
+erase("biblio.bib","          ","    A")
